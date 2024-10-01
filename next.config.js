ff95abdf28b39d7b2d0c1ae8123800c341afe93b
Tuanxu5 <lehoangtuan012@gmail.com>
2024-10-01 15:47:05 +0700

    add export

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  serverExternalPackages: ["package-name"],
+  experimental: {
+    optimizePackageImports: ["icon-library"]
+  }
+}
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true"

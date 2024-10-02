3ebde8ea0bc0c2666145015ff46a030d05957866
tuanxu5 <lehoangtuan012@gmail.com>
2024-10-02 08:58:21 +0700

    format font

--- src/app/components/shared/title/index.tsx
+++ src/app/components/shared/title/index.tsx
@@ -10,27 +10,9 @@ export default function TitleComponent({ title, subTitle, name, ...rest }: Title
       <div className="w-full">
         <div className="flex justify-between items-center">
           <h5 className="text-[18px] font-medium mb-10">{title}</h5>
-          {/* {isLink && (
-            <Link href={link} className="font-semibold text-right flex items-center justify-end gap-1 text-15px">
-              <svg
-                data-slot="icon"
-                fill="none"
-                stroke-width="3"
-                stroke="currentColor"
-                width="15"
-                height="15"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-              >
-                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5"></path>
-              </svg>
-              <span>{titleLink}</span>
-            </Link>
-          )} */}
         </div>
         <h1 className="text-[60px] font-bold leading-[70px]">{name}</h1>
-        <h1 className="text-[45px] font-bold leading-[50px]">
+        <h1 className="text-[45px] font-semibold leading-[50px]">
           <div>{subTitle}</div>
         </h1>
       </div>
--- src/app/sections/skills/index.tsx
+++ src/app/sections/skills/index.tsx
@@ -11,14 +11,14 @@ export default function SkillsSection() {
           <div className="col-span-6 text-center text-3xl mb-5 font-semibold flex justify-center ">
             <div className="px-5 py-3 rounded-2xl bg-[#26a0f81a] flex justify-center gap-2 items-center mb-5">
               <span className="icon_all icon-tech-stack"></span>
-              <h1 className="text-2xl font-bold bg-gradient-to-br from-blue-500 to-blue-300 text-transparent bg-clip-text">
+              <h1 className="text-2xl font-semibold bg-gradient-to-br from-blue-500 to-blue-300 text-transparent bg-clip-text">
                 Tech Stack
               </h1>
             </div>
           </div>
           <div className="grid grid-cols-6 gap-x-5 gap-y-10">
             {skill?.children?.map((item) => (
-              <div key={item.id} className="flex flex-col items-center gap-2.5 text-sm font-bold">
+              <div key={item.id} className="flex flex-col items-center gap-2.5 text-sm font-semibold">
                 <div className="bg-[#f4f6f7] rounded-2xl w-14 h-14 flex justify-center items-center">
                   <Image src={item.image} height={item.height} width={item.width} alt={item.name} />
                 </div>

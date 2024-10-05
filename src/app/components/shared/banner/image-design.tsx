717a3f362a19e05cdabc10d562eaa970f5aee868
tuanxu5 <lehoangtuan012@gmail.com>
2024-10-05 15:46:27 +0700

    add key

--- src/app/components/shared/banner/image-design.tsx
+++ src/app/components/shared/banner/image-design.tsx
@@ -1,65 +1,58 @@
+import Image from "next/image";
+
 export const ImageDesign = () => {
   return (
     <div className="relative">
-      <img
+      <Image
         alt="Web development"
-        srcSet="https://ronasit.com/_next/static/media/android-development-light.845e0b23.svg"
         src="https://ronasit.com/_next/static/media/android-development-light.845e0b23.svg"
         decoding="async"
         className="relative"
       />
-      <img
+      <Image
         alt="Cloud"
-        srcSet="https://ronasit.com/_next/static/media/cargo-light.0d07f90b.svg"
         src="https://ronasit.com/_next/static/media/cargo-light.0d07f90b.svg"
         decoding="async"
         className="absolute top-[40%] right-[8%]"
       />
-      <img
+      <Image
         alt="Mouse"
-        srcSet="https://ronasit.com/_next/static/media/robothead-light.7691d8c2.svg"
         src="https://ronasit.com/_next/static/media/robothead-light.7691d8c2.svg"
         decoding="async"
         className="absolute transform -translate-x-1/2 bottom-[15%] left-[10%]"
       />
-      <img
+      <Image
         alt="Mouse"
-        srcSet="https://ronasit.com/_next/static/media/ui-light-3.8961f5c5.svg"
         src="https://ronasit.com/_next/static/media/ui-light-3.8961f5c5.svg"
         decoding="async"
         className="absolute transform -translate-x-1/2 bottom-[11%] left-[48%]"
       />
-      <img
+      <Image
         alt="Mouse"
-        srcSet="https://ronasit.com/_next/static/media/ui-light-2.1f09819e.svg"
         src="https://ronasit.com/_next/static/media/ui-light-2.1f09819e.svg"
         decoding="async"
         className="absolute transform -translate-x-1/2 bottom-[20%] left-[47%]"
       />
-      <img
+      <Image
         alt="Mouse"
-        srcSet="https://ronasit.com/_next/static/media/ui-light-1.1538a2f4.svg"
         src="https://ronasit.com/_next/static/media/ui-light-1.1538a2f4.svg"
         decoding="async"
         className="absolute transform -translate-x-1/2 bottom-[30%] left-[48%]"
       />
-      <img
+      <Image
         alt="Mouse"
-        srcSet="https://ronasit.com/_next/static/media/rectangle-small-light-2.2b8e6f4a.svg"
         src="https://ronasit.com/_next/static/media/rectangle-small-light-2.2b8e6f4a.svg"
         decoding="async"
         className="absolute transform -translate-x-1/2 bottom-[10%] right-[38.7%]"
       />
-      <img
+      <Image
         alt="Mouse"
-        srcSet="https://ronasit.com/_next/static/media/rectangle-large-light-3.ca6e8538.svg"
         src="https://ronasit.com/_next/static/media/rectangle-large-light-3.ca6e8538.svg"
         decoding="async"
         className="absolute transform -translate-x-1/2 bottom-[5%] right-[11%]"
       />
-      <img
+      <Image
         alt="Mouse"
-        srcSet="https://ronasit.com/_next/static/media/search-light.c4fa6daa.svg"
         src="https://ronasit.com/_next/static/media/search-light.c4fa6daa.svg"
         decoding="async"
         className="absolute transform -translate-x-1/2 bottom-[38%] right-[14%]"
--- src/app/components/shared/banner/image-web.tsx
+++ src/app/components/shared/banner/image-web.tsx
@@ -1,44 +1,40 @@
+import Image from "next/image";
+
 export const ImageWeb = () => {
   return (
     <div className="mt-[100px] relative">
-      <img
+      <Image
         alt="Web development"
-        srcSet="https://ronasit.com/_next/static/media/web-development-light.70611adc.svg"
         src="https://ronasit.com/_next/static/media/web-development-light.70611adc.svg"
         decoding="async"
         className="relative"
       />
-      <img
+      <Image
         alt="Cloud"
-        srcSet="https://ronasit.com/_next/static/media/cloud-light.1b9a7bfb.svg"
         src="https://ronasit.com/_next/static/media/cloud-light.1b9a7bfb.svg"
         decoding="async"
         className="absolute top-[10%] left-[5%]"
       />
-      <img
+      <Image
         alt="Angular"
-        srcSet="https://ronasit.com/_next/static/media/angular-light.69df12c5.svg"
         src="https://ronasit.com/_next/static/media/angular-light.69df12c5.svg"
         decoding="async"
         className="absolute right-[20%] top-[-10%]"
       />
-      <img
+      <Image
         alt="React"
-        srcSet="https://ronasit.com/_next/static/media/react-light.6e3745ce.svg"
         src="https://ronasit.com/_next/static/media/react-light.6e3745ce.svg"
         decoding="async"
         className="absolute bottom-[30%] right-[25%]"
       />
-      <img
+      <Image
         alt="Mouse"
-        srcSet="https://ronasit.com/_next/static/media/mouse-light.105213fe.svg"
         src="https://ronasit.com/_next/static/media/mouse-light.105213fe.svg"
         decoding="async"
         className="absolute transform -translate-x-1/2 bottom-7 left-[60%]"
       />
-      <img
+      <Image
         alt="Java"
-        srcSet="https://ronasit.com/_next/static/media/java-light.2ecf81de.svg"
         src="https://ronasit.com/_next/static/media/java-light.2ecf81de.svg"
         decoding="async"
         className="absolute right-[40%] top-0"

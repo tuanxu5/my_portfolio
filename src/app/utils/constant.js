455e267e891e92919f367066917acc39b59320ff
Tuanxu5 <lehoangtuan012@gmail.com>
2025-01-14 17:15:12 +0700

    update

--- src/app/utils/constant.js
+++ src/app/utils/constant.js
@@ -1,3 +1,3 @@
-export const LINK_CV = "https://drive.google.com/file/d/17L2w4xwv94PEyIy4VsiZmQtjQgtXWXTp/view?usp=sharing";
+export const LINK_CV = "https://drive.google.com/file/d/13-ZWO2BJ-ehY0XmvT4NW16Y5RB-g9j3d/view";
 
 export const PADDING_TOP_PAGE = 160;
